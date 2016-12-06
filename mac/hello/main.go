446ff250f632f6771c4b2d3243bfa2510fda4fea
Maxence Charriere <charriere@outlook.com>
2016-12-05 18:53:54 -0800

    update for reopen

--- mac/hello/main.go
+++ mac/hello/main.go
@@ -5,22 +5,42 @@ import (
 	_ "github.com/murlokswarm/mac"
 )
 
+var (
+	win app.Contexter
+)
+
 func main() {
 	app.OnLaunch = func() {
 		appMenu := &AppMainMenu{} // Creates the AppMainMenu component.
 		app.Menu().Mount(appMenu) // Mounts the AppMainMenu component into the application menu context.
+		win = newMainWindow()
+	}
 
-		// Creates a window context.
-		win := app.NewWindow(app.Window{
-			Title:          "Hello World",
-			Width:          1280,
-			Height:         720,
-			TitlebarHidden: true,
-		})
+	app.OnReopen = func(hasVisibleWindow bool) {
+		if win != nil {
+			return
+		}
 
-		hello := &Hello{} // Creates a Hello component.
-		win.Mount(hello)  // Mounts the Hello component into the window context.
+		win = newMainWindow()
 	}
 
 	app.Run()
+}
+
+func newMainWindow() app.Contexter {
+	// Creates a window context.
+	win := app.NewWindow(app.Window{
+		Title:          "Hello World",
+		Width:          1280,
+		Height:         720,
+		TitlebarHidden: true,
+		OnClose: func() bool {
+			win = nil
+			return true
+		},
+	})
+
+	hello := &Hello{} // Creates a Hello component.
+	win.Mount(hello)  // Mounts the Hello component into the window context.
+	return win
 }
